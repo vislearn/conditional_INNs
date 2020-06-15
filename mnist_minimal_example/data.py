7f4d5d9a9386a1fcb50fcf635b05c130d0747a98
Peter Steinbach <p.steinbach@hzdr.de>
2020-06-15 12:08:21 +0200

    training can be performed on the CPU as well

--- mnist_minimal_example/data.py
+++ mnist_minimal_example/data.py
@@ -10,27 +10,37 @@ data_std = 0.305
 # amplitude for the noise augmentation
 augm_sigma = 0.08
 data_dir = 'mnist_data'
+_default_device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 def unnormalize(x):
     '''go from normaized data x back to the original range'''
     return x * data_std + data_mean
 
 
-train_data = torchvision.datasets.MNIST(data_dir, train=True, download=True,
-                                        transform=T.Compose([T.ToTensor(), lambda x: (x - data_mean) / data_std]))
-test_data = torchvision.datasets.MNIST(data_dir, train=False, download=True,
-                                        transform=T.Compose([T.ToTensor(), lambda x: (x - data_mean) / data_std]))
+def setup(device=_default_device,
+          batchsize=batch_size,
+          mean=data_mean,
+          std=data_std,
+          folder=data_dir,
+          augm_std=augm_sigma):
 
-# Sample a fixed batch of 1024 validation examples
-val_x, val_l = zip(*list(train_data[i] for i in range(1024)))
-val_x = torch.stack(val_x, 0).cuda()
-val_l = torch.LongTensor(val_l).cuda()
+    train_data = torchvision.datasets.MNIST(folder, train=True, download=True,
+                                            transform=T.Compose([T.ToTensor(), lambda x: (x - mean) / std]))
+    test_data = torchvision.datasets.MNIST(folder, train=False, download=True,
+                                            transform=T.Compose([T.ToTensor(), lambda x: (x - mean) / std]))
 
-# Exclude the validation batch from the training data
-train_data.data = train_data.data[1024:]
-train_data.targets = train_data.targets[1024:]
-# Add the noise-augmentation to the (non-validation) training data:
-train_data.transform = T.Compose([train_data.transform, lambda x: x + augm_sigma * torch.randn_like(x)])
+    # Sample a fixed batch of 1024 validation examples
+    val_x, val_l = zip(*list(train_data[i] for i in range(1024)))
+    val_x = torch.stack(val_x, 0).to(device)
+    val_l = torch.LongTensor(val_l).to(device)
 
-train_loader  = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True, drop_last=True)
-test_loader   = DataLoader(test_data,  batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True, drop_last=True)
+    # Exclude the validation batch from the training data
+    train_data.data = train_data.data[1024:]
+    train_data.targets = train_data.targets[1024:]
+    # Add the noise-augmentation to the (non-validation) training data:
+    train_data.transform = T.Compose([train_data.transform, lambda x: x + augm_std * torch.randn_like(x)])
+
+    train_loader  = DataLoader(train_data, batch_size=batchsize, shuffle=True, num_workers=4, pin_memory=True, drop_last=True)
+    test_loader   = DataLoader(test_data,  batch_size=batchsize, shuffle=False, num_workers=4, pin_memory=True, drop_last=True)
+
+    return train_loader, test_loader, val_x, val_l
